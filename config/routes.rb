6ca248c237d5c8574ba7cb7d92f25167f4ffacce
Pierre Girbau <pierregirbau@gmail.com>
2018-02-13 15:08:16 +0100

    Add edit function and corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
 
   # #UPDATE
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'get_task'
-  patch 'tasks', to: 'tasks#update', as: 'edit_task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
 
   # #DELETE
   delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
