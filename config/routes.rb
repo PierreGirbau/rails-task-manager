2855f7d5bd5e0f29d6ccb1b8abd4394a27298009
Pierre Girbau <pierregirbau@gmail.com>
2018-02-13 15:53:19 +0100

    Refactored code using resources, before_action and _form partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
   #CREATE
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  # #READ
-  # #Read one
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-  # #Read all
-  get 'tasks', to: 'tasks#index'
+  # # #READ
+  # # #Read one
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # #Read all
+  # get 'tasks', to: 'tasks#index'
 
-  # #UPDATE
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'tasks#update'
+  # # #UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # #DELETE
-  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+  # # #DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
